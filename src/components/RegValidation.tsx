44030261309bb718ffcb6e492f48e8b964c61650
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-20 17:44:10 +0300

    take value from MUI textfield

--- src/components/RegValidation.tsx
+++ src/components/RegValidation.tsx
@@ -1,18 +1,38 @@
+import { useState } from 'react';
 import { TextField, Typography } from '@material-ui/core';
 import useStyles from './styles';
 
+import isValidRegEmail from './utils/isValidRegEmail';
+
 export const RegValidation = () => {
     const classes = useStyles();
+    
+    const [ email, setEmail ] = useState('');
+    console.log(email)
+   
     return (
         <section>
             <Typography variant="h2">
                 Form validation with Regex
             </Typography>
             <form className={classes.form} noValidate autoComplete="off">
-  <TextField className={classes.txtField} id="standard-basic" label="Standard" />
-  <TextField className={classes.txtField} id="filled-basic" label="Filled" variant="filled" />
-  <TextField className={classes.txtField} id="outlined-basic" label="Outlined" variant="outlined" />
-</form>
+                <TextField 
+                className={classes.txtField} 
+                id="standard-basic" 
+                label="Standard"
+                value={email}
+                onChange={(event) => {setEmail(event.target.value)}} />
+                <TextField 
+                className={classes.txtField} 
+                id="filled-basic" 
+                label="Filled" 
+                variant="filled" />
+                <TextField 
+                className={classes.txtField} 
+                id="outlined-basic" 
+                label="Outlined" 
+                variant="outlined" />
+            </form>
         </section>
     )
 };
