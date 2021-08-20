854b3cbbbf1ee3f6de01267f6cb1fa63f5a9a971
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-20 16:59:23 +0300

    add regvalidation skeleton

--- src/components/RegValidation.tsx
+++ src/components/RegValidation.tsx
@@ -1,5 +1,18 @@
+import { TextField, Typography } from '@material-ui/core';
+import useStyles from './styles';
+
 export const RegValidation = () => {
+    const classes = useStyles();
     return (
-        <div>Hello</div>
+        <section>
+            <Typography variant="h2">
+                Form validation with Regex
+            </Typography>
+            <form className={classes.form} noValidate autoComplete="off">
+  <TextField className={classes.txtField} id="standard-basic" label="Standard" />
+  <TextField className={classes.txtField} id="filled-basic" label="Filled" variant="filled" />
+  <TextField className={classes.txtField} id="outlined-basic" label="Outlined" variant="outlined" />
+</form>
+        </section>
     )
 };
