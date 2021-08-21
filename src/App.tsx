4889bbbabb0b59ce4b85b929b7121982f982efae
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-21 13:00:09 +0300

    add component with email-validator package

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { RegValidation } from './components/RegValidation'
 import { HtmlValidation } from './components/HtmlValidation';
+import { NpmEmailValidator } from './components/NpmEmailValidator';
 
 function App() {
   return (
     <div className="App">
       <RegValidation />
       <HtmlValidation />
+      <NpmEmailValidator />
     </div>
   );
 }
