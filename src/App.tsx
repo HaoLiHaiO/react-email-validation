c875761b153dc9b6d908cfaa80f323fe6fee52f4
HaoLiHaiO <pinelwebdevelopment@gmail.com>
2021-08-21 12:27:35 +0300

    add html validator to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { RegValidation } from './components/RegValidation'
+import { HtmlValidation } from './components/HtmlValidation';
 
 function App() {
   return (
     <div className="App">
       <RegValidation />
+      <HtmlValidation />
     </div>
   );
 }
